7731ef3d5ca316bf6eec7481a1a068466b3c5659
Guray Ozarslan <guray5285@hotmail.com>
2022-06-09 09:48:16 +0300

    fx

--- src/pages/AllMeetups.js
+++ src/pages/AllMeetups.js
@@ -1,20 +1,34 @@
 import { useState } from "react";
+import { useEffect } from "react";
 import MeetupList from "../components/meetups/MeetupList";
 
 function AllMeetupsPage() {
   const [isLoading, setIsLoading] = useState(false);
   const [loadedMeetups, setIsLoadedMeetups] = useState([]);
 
-  fetch(
-    "https://react-getting-started-b1f1d-default-rtdb.firebaseio.com/meetups.json"
-  )
-    .then((response) => {
-      response.json();
-    })
-    .then((data) => {
-      setIsLoading(false);
-      setIsLoadedMeetups(data);  
-    });
+  useEffect(() => {
+    setIsLoading(true);
+    fetch(
+      "https://react-getting-started-b1f1d-default-rtdb.firebaseio.com/meetups.json"
+    )
+      .then((response) => {
+        response.json();
+      })
+      .then((data) => {
+        const meetups = [];
+
+        for (const key in data) {
+          const meetup = {
+            id: key,
+            ...data[key],
+          };
+
+          meetups.push(meetup);
+        }
+        setIsLoading(false);
+        setIsLoadedMeetups(meetups);
+      });
+  }, []);
 
   if (isLoading) {
     return (
